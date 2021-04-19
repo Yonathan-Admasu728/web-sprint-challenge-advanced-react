77e6d4d402fb28fb97ba64c019340c33d9f7c6c7
yonathan Admasu <admyoni4@yahoo.com>
2021-04-18 19:49:31 -0700

    worked on tests

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -4,6 +4,13 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+    const header = screen.getByText(/reac plants/i)
+    expect(header).toBeInTheDocument()
+    expect(header).toBeTruthy()
+    expect(header).toHaveTextContent("Contact Form")
+
+});
 
 test("form shows success message on submit with form details", () => {});
